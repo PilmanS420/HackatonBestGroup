e11488aaa0c07d650ce2aaa292d3cd4c5907052f
egorv <egorviktorov@outlook.com>
2023-02-22 17:33:09 +0200

    Classes directory and Button class added

---
+++ classes/Button.py
@@ -0,0 +1,25 @@
+class Button:
+    """
+    A class used to represent a Button on the screen
+    """
+    def __init__(self, x_pos, y_pos, width, height):
+        """
+        Constructor
+
+        :param x_pos: int
+            Position of the top left corner of the button in X axis
+        :param y_pos: int
+            Position of the top left corner of the button in Y axis
+        :param width: int
+            Width of button in pixels
+        :param height: int
+            Height of button in pixels
+        """
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.width = width
+        self.height = height
+
+    def get_pressed(self, mouse_pos):
+        return self.x_pos <= mouse_pos[0] < self.x_pos + self.width\
+               and self.y_pos <= mouse_pos[1] < self.y_pos + self.height
