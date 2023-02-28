216528b553434139ab05b411c00dcb84e28fa80f
Aviel <avielpiven0204@gmail.com>
2023-02-28 17:38:20 +0200

    Added menu buttons dictionary

--- buttons.py
+++ buttons.py
@@ -5,3 +5,4 @@ from constants import *
 
 # Dictionary for buttons for moving from screen to screen:
 screen_button_dictionary = {"customer": Button(*SCREEN_BUTTON_CUSTOMER_LOCATION), "precooking": Button(*SCREEN_BUTTON_PRECOOKING_LOCATION), "cooking": Button(*SCREEN_BUTTON_COOKING_LOCATION), "finishing": Button(*SCREEN_BUTTON_FINISHING_LOCATION)}
+menu_button_dictionary = {"start": Button(*MENU_BUTTON_START), "exit": Button(*MENU_BUTTON_EXIT)}
